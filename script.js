8602d36c4f7888cefd83a50b461d6d6ea15b7f51
Adrian <kmhorn1992@icloud.com>
2017-10-03 15:31:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var sentences = [
+    'In West Philadelphia, born and raised',
+    'On the playground is where I spent most of my days',
+    'Chillin out, maxin, relaxin all cool',
+    'And all shootin some b-ball outside of the school',
+    'When a couple of guys who were up to no good',
+    'Started makin trouble in my neighborhood',
+    'I got in one lil fight and my mom got scared',
+    'said "Youre moving with your auntie and uncle in Bel-Air"',
+];
+
+$(document).ready(function(){
+    $("#keyboard-upper-container").hide();
+        
+
+  
+});
+
