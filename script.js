9dfb8b48478f5f86d41da469dd0bcbbe83b941a9
Adrian <kmhorn1992@icloud.com>
2017-10-03 16:52:15 -0500

    second commit

--- script.js
+++ script.js
@@ -6,13 +6,60 @@ var sentences = [
     'When a couple of guys who were up to no good',
     'Started makin trouble in my neighborhood',
     'I got in one lil fight and my mom got scared',
-    'said "Youre moving with your auntie and uncle in Bel-Air"',
+    'said youre moving with your auntie and uncle in Bel-Air',
 ];
+var upkey,
+    lowkey;
+    sentencecounter = 0
+    charactercounrer
 
-$(document).ready(function(){
-    $("#keyboard-upper-container").hide();
-        
-
-  
-});
-
+    $(document).ready(() => {
+        upkey = $('#keyboard-upper-container');
+        lowkey = $('#keyboard-lower-container');
+    
+        toggleKeyboards();
+    
+        $(document).on('keyup keydown', (e) => {
+            handleKeyPress(e.type, e.shiftKey, e.keyCode);
+        });
+    });
+    
+    function handleKeyPress(type, isShift, keyCode) {
+        if (keyCode === 16) {
+            if (type === 'keydown') {
+                toggleKeyboards(true);
+            } else {
+                toggleKeyboards();
+            }
+        }
+    
+        var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
+    
+        handleHighlight(trueKeyCode, type);
+    }
+    
+    function handleHighlight(keyCode, type) {
+        if (type === 'keydown') {
+            $('#' + keyCode).css('background-color', 'blue');
+        } else {
+            $('#' + keyCode).css('background-color', '#f5f5f5');
+        }
+    }
+    
+    function configureTrueKeyCode(keyCode, isShift) {
+        if (isShift === false && keyCode >= 65 && keyCode <= 90) {
+            return keyCode + 32;
+        }
+    
+        return keyCode;
+    }
+    
+    function toggleKeyboards(showUpKey) {
+        if (showUpKey) {
+            lowkey.hide();
+            upkey.show();
+        } else {
+            upkey.hide();
+            lowkey.show();
+        }
+    }
