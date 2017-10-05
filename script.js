6f35b3e8179b7b2642afdda1998b0a11b107c5c8
Adrian <kmhorn1992@icloud.com>
2017-10-05 10:30:50 -0500

    getting there

--- script.js
+++ script.js
@@ -10,51 +10,29 @@ var sentences = [
 ];
 var upkey,
     lowkey;
-    sentencecounter = 0
-    charactercounrer
+sentencecounter = 0;
 
-    $(document).ready(() => {
-        upkey = $('#keyboard-upper-container');
-        lowkey = $('#keyboard-lower-container');
+
+$(document).ready(() => {
+    //variables for keyboard//
+    upkey = $('#keyboard-upper-container');
+    lowkey = $('#keyboard-lower-container');
     
-        toggleKeyboards();
     
-        $(document).on('keyup keydown', (e) => {
-            handleKeyPress(e.type, e.shiftKey, e.keyCode);
-        });
+if ('#keyboard-upper-container') {
+    upkey.hide();        
+        
+}
+   
+    $(document).on('keyup keydown', (e) => {
+        handleKeyPress(e.type, e.shiftKey, e.keyCode);
     });
-    
-    function handleKeyPress(type, isShift, keyCode) {
-        if (keyCode === 16) {
-            if (type === 'keydown') {
-                toggleKeyboards(true);
-            } else {
-                toggleKeyboards();
-            }
-        }
-    
-        var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
-    
-        handleHighlight(trueKeyCode, type);
-    }
-    
-    function handleHighlight(keyCode, type) {
-        if (type === 'keydown') {
-            $('#' + keyCode).css('background-color', 'blue');
-        } else {
-            $('#' + keyCode).css('background-color', '#f5f5f5');
-        }
-    }
-    
-    function configureTrueKeyCode(keyCode, isShift) {
-        if (isShift === false && keyCode >= 65 && keyCode <= 90) {
-            return keyCode + 32;
-        }
-    
-        return keyCode;
-    }
-    
+});
+
+toggleKeyboards();
+
     function toggleKeyboards(showUpKey) {
+        
         if (showUpKey) {
             lowkey.hide();
             upkey.show();
@@ -63,3 +41,34 @@ var upkey,
             lowkey.show();
         }
     }
+    function handleKeyPress(type, isShift, keyCode) {
+        if (keyCode === 16) {
+            if (type === 'keydown') {
+                toggleKeyboards(true);
+            } else {
+                toggleKeyboards();
+            }
+        }
+       
+        var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
+
+        handleHighlight(trueKeyCode, type);
+    }
+
+    function handleHighlight(keyCode, type) {
+        if (type === 'keydown') {
+            $('#' + keyCode).css('background-color', 'blue');
+        } else {
+            $('#' + keyCode).css('background-color', '#f5f5f5');
+        }
+    }
+
+    function configureTrueKeyCode(keyCode, isShift) {
+        if (isShift === false && keyCode >= 65 && keyCode <= 90) {
+            return keyCode + 32;
+        }
+
+        return keyCode;
+    }
+
+   
